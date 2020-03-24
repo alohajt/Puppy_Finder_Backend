3d7ad323788f927921a599223d19580c58e2b8d6
alohajt <alohajiatao@gmail.com>
2020-03-24 14:44:27 -0600

    github bug

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,22 +3,22 @@ class Animal < ApplicationRecord
   # belongs_to :user, optional:true
   has_many :users, through: :favorites
 
-  def favorites
-    Favorite.all.select{|n|n.animal_id == self.id}
-  end
+  # def favorites
+  #   Favorite.all.select{|n|n.animal_id == self.id}
+  # end
   
-  def favorite_users
-      user_ids = favorites().map{|n|n.user_id}
-      count=0
-      User.all.reduce([]) do |acc , user|
-        # byebug
-        if user.id==user_ids[count]
-          acc << user
-        else
-          acc=acc
-        end
-        count = count +1
-      end
-  end
+  # def favorite_users
+  #     user_ids = favorites().map{|n|n.user_id}
+  #     count=0
+  #     User.all.reduce([]) do |acc , user|
+  #       # byebug
+  #       if user.id==user_ids[count]
+  #         acc << user
+  #       else
+  #         acc=acc
+  #       end
+  #       count = count +1
+  #     end
+  # end
 
 end
