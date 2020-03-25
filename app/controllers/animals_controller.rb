65b5c002582db4fd29d55baf9b14b06fe46dfaf6
jjhiggz <jonathan.higger@gmail.com>
2020-03-25 10:24:27 -0600

    first commit

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,10 @@
 class AnimalsController < ApplicationController
+  def index
+    animals = Animal.all
+    render json: animals, include: [:favorites, :users, :location]
+  end
+
+  def show
+    
+  end
 end
